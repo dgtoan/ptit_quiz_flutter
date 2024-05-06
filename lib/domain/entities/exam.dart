7fbfb247de78a434ea3d78cf828d5ec27f5adff7
dgtoan <dgtoan28@gmail.com>
2024-05-07 06:32:49 +0700

    feat: copyWith method

--- lib/domain/entities/exam.dart
+++ lib/domain/entities/exam.dart
@@ -14,4 +14,27 @@ class Exam {
     required this.start,
     this.questions,
   });
+
+  const Exam.empty()
+      : id = '',
+        name = '',
+        duration = 0,
+        start = 0,
+        questions = null;
+
+  Exam copyWith({
+    String? id,
+    String? name,
+    int? duration,
+    int? start,
+    List<Question>? questions,
+  }) {
+    return Exam(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      duration: duration ?? this.duration,
+      start: start ?? this.start,
+      questions: questions ?? this.questions,
+    );
+  }
 }
